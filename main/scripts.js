d1b8b2a106a284bcd7042a8ed1a7669febbf65a5
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-04 09:53:25 +0000

    Listening for the keypress event

--- main/scripts.js
+++ main/scripts.js
@@ -2,6 +2,7 @@ var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
 
 
 function setDetails(imageUrl,titleText){
@@ -49,10 +50,21 @@ function hideDetails(){
     document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
+function addKeyPressHandler(){
+    'use strict';
+    document.body.addEventListener('keyup',function(event){
+        event.preventDefault();
+        if(event.keyCode === ESC_KEY){
+            hideDetails();
+        }
+    })
+}
+
 function initializeEvents(){
     'use strict';
     var thumbnails = getTumbnailsArray();
     thumbnails.forEach(addThumbClickHandler)
+    addKeyPressHandler();
 }
 
 initializeEvents();
